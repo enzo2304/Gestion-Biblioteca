06273d56568486e0773df3053f17b41a41c4eecd
Enzo <enzo-2304@hotmail.com>
2025-10-25 14:41:26 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -21,4 +21,45 @@ public class Biblioteca {
    private void setNombre(String p_nombre){
        this.nombre = p_nombre;
    }
+   private void setLibros(ArrayList<Libro> p_libros){
+       this.libros = p_libros;
+   }
+   private void setSocio(HashMap<Integer,Socio> p_socios){
+       this.socios = p_socios;
+   }
+   
+   //getter
+   public String getNombre(){
+       return this.nombre;
+   }
+   public HashMap<Integer,Socio> getSocios(){
+       return this.socios;
+   }
+   public ArrayList<Libro> getLibros(){
+       return this.libros;
+   }
+   
+   //agregar libros o socios
+   
+   public void agregarLibro(Libro p_libro){
+       this.getLibros().add(p_libro);
+   }
+   public void agregarSocio(Socio p_socio){
+       //aca vamos a tener que ver como le llaman al getDni o getDniSocio
+       this.getSocios().put(new Integer(p_socio.getDni) , p_socio);
+   }
+   
+   
+   //eliminar libro o socio
+   public void quitarLibro(Libro p_libro){
+       this.getLibros().remove(p_libro);
+   }
+   public void eliminarSocio(Socio p_socio){
+       this.getSocios().remove(new Integer(p_socio.getDni));
+   }
+   
+   public void nuevoLibro(String p_titulo, int p_edicion,String p_editorial, int p_anio){
+       //aca el mismo dilema de como acomodan los parametros en el constructor 
+       this.agregarLibro(new Libro(p_titulo,p_edicion,p_editorial,p_anio));
+   }
 }
